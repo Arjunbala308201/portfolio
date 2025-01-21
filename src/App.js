70e029cf65290810b32f43a5bb10dc3a122a0c97
Arjunbala308201 <saarjun04@gmail.com>
2025-01-21 21:45:07 +0530

    removed toasts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Correct usage of BrowserRouter
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Main } from './components/Main';
 import { Home } from './components/Home';
 import { Education } from './components/Education';
 import 'aos/dist/aos.css'; // Import the AOS styles
-import AOS from 'aos';     // Import AOS JavaScript
 import { Skills } from './components/Skills';
 import { Project } from './components/Project';
 
